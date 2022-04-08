7877a120b15b928240ab73ca52507def3c808a76
Ash Omaraie <aomarai@ilstu.edu>
2022-04-08 17:06:46 +0000

    Added data files and other edits

--- Assignment 6/src/a4_app.cpp
+++ Assignment 6/src/a4_app.cpp
@@ -13,9 +13,29 @@ using namespace std;
 
 int main()
 {
-    cout << "Assignment 04 (Your Name):" << endl;
+    string id, firstName, lastName, department, gpa;
+    cout << "Assignment 06 (Ashkan Omaraie):" << endl;
     
-    // Add your code here:
-    return 0;
+    //Read the text file enroll_test.txt in the data folder
+    ifstream file("../data/enroll_test.txt");
 
+    //Create hash table of student objects with 7 buckets
+    HashTable<string> ht(7);
+
+    //Read the file line by line. The first word is the id, second word is firstName, third word is lastname, fourth word is department, fifth word is gpa
+    while (file >> id >> firstName >> lastName >> department >> gpa)
+    {
+        //Create a student object with the id, firstName, lastName, department, gpa
+        Student<string> tempStudent;
+        tempStudent.setFirstName(firstName);
+        tempStudent.setLastName(lastName);
+        tempStudent.setDepartment(department);
+        tempStudent.setGPA(gpa);
+
+        //Insert the student object to the hash table using the id as the key
+        ht.insert(id, tempStudent);
+    }
+
+    //Print out all the buckets in the hash table
+    ht.displayHash();
 }
