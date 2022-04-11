c239641a98478ca5b1c98d7c1ee14914e81ad27c
Ash Omaraie <aomarai@ilstu.edu>
2022-04-11 03:38:07 +0000

    Added assignment 7

---
+++ Assignment 7/it279_a07_q2/a7_q2_app.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <functional>
+#include <array>
+#include <cassert>
+#include <random>
+#include <sstream> // std::stringstream
+#include <fstream>
+#include "BinaryHeap.h"
+#include "BinaryHeap.cpp"
+#include "flags.h"
+#include "Student.h"
+
+#include "Student.cpp"
+
+using namespace std;
+
+// Test program
+int main()
+{
+    cout << "\nAssignment 07, Question 02 (Your Name): " << endl;
+	// Add your code here:
+
+
+    return 0;
+}
